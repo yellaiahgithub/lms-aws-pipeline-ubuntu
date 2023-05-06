d861dc1997f045c6408ffb61be65372127eddc0a
yellaiahgithub <105124131+yellaiahgithub@users.noreply.github.com>
2023-05-06 17:54:19 +0530

    Delete before_install.sh

--- before_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&sudo apt-get install -y nodejs
-sudo apt update -y
-sudo apt install nginx -y 
-sudo systemctl restart nginx
