a75e95e630d95fc31facd5e1f8b8be11c18db73a
yellaiahgithub <105124131+yellaiahgithub@users.noreply.github.com>
2023-05-09 19:28:30 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-cd /home/ubuntu
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
-sudo apt-get install -y nodejs 
-sudo apt install nginx -y
+cd /home/ec2-user
+curl -fsSL https://rpm.nodesource.com/setup_16.x | sudo bash -
+yum -y install nodejs npm
+sudo yum update -y
+sudo amazon-linux-extras install nginx1 -y
+sudo amazon-linux-extras enable nginx1
 sudo systemctl start nginx
